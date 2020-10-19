d444f10ad0696503f6325c91e09d95ac85f90c8e
Leo <leo@MacBook-Pro-de-Leo-3.local>
2020-10-19 23:18:27 +0200

    Add react-chartjs-2 and first test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Bar, Doughnut } from 'react-chartjs-2';
+import BarExample from '../src/components/bar'
+
+const data = {
+    labels: [
+        'Red',
+        'Green',
+        'Yellow'
+    ],
+    datasets: [{
+        data: [300, 50, 100],
+        backgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56'
+        ],
+        hoverBackgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56'
+        ]
+    }]
+};
 
 function App() {
   return (
@@ -8,17 +31,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Morphee
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+        <Doughnut data={data}/>
+        <BarExample/>
     </div>
   );
 }
