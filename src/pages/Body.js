247719d9a69ea6143209a01c5840d9e7f1abeee3
Leo <leo@MacBook-Pro-de-Leo-3.local>
2021-03-12 12:02:46 +0100

    Design quickfix on page effet sur le corps

--- src/pages/Body.js
+++ src/pages/Body.js
@@ -94,30 +94,30 @@ class Body extends Component {
 
                 <div className="body-content col-12">
                     <div>
-                        <h1>Les efffets du manque sommeil sur votre corps</h1>
+                        <h1>Les effets du manque sommeil sur votre corps</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc gravida libero quis dolor elementum aliquam. Aenean vulputate nisi et eros fermentum, in pellentesque ipsum pharetra. Quisque egestas sollicitudin accumsan.</p>
                     </div>
                     <form onSubmit={this.handleSubmit}>
                         <label>
                             Temps de sommeil:
+                        </label>
                             <select value={this.state.value} onChange={this.handleChange}>
                                 <option selected value="1">moins de 6 heures</option>
                                 <option value="2">entre 6 à 8 heures</option>
                                 <option value="3">Plus de 8 heures</option>
                             </select>
-                        </label>
-                        <input type="submit" value="Envoyer" />
+
+                        <input className="submit" type="submit" value="Envoyer" />
                     </form>
                     <div className="container-img">
                         <img src={body_woman} className="body_img" alt="Corps humain" />
-
                         <div className="block block-1" onMouseEnter={this.handleMouseHover}
                              onMouseLeave={this.handleMouseHover}>
                             {
                                 this.state.isHovering &&
                                 <div className="text-block text-block-1">
-                                    <h5>Maladie 1</h5>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    <h5>Usure du cerveau</h5>
+                                    <p>Lorsque nous dormons, le cerveau régénère les cellules qui lui permettent de “récupérer les énergies” et prévenir les maladies. Si nous ne nous reposons pas assez, il ne peut pas nettoyer les protéines qui provoquent les plaques d’Alzheimer ou la démence.</p>
                                 </div>
                             }
                         </div>
@@ -127,8 +127,8 @@ class Body extends Component {
                             {
                                 this.state.isHovering2 &&
                                 <div className="text-block text-block-2">
-                                    <h5>Maladie 2</h5>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    <h5>Manque de réflexes</h5>
+                                    <p>Le rendement cognitif et la coordination motrice diminuent de façon considérable quand une personne n’a pas suffisamment dormi. Selon la National Highway Traffic Safety Administration (NHTSA, en anglais) des États-Unis, plus de quarante mille personnes souffrent de blessures chaque année. Et ce, à cause de problèmes dus au manque de sommeil.</p>
                                 </div>
                             }
                         </div>
@@ -138,8 +138,8 @@ class Body extends Component {
                             {
                                 this.state.isHovering3 &&
                                 <div className="text-block text-block-2">
-                                    <h5>Maladie 3</h5>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    <h5>Meilleur rendement</h5>
+                                    <p>Comme conséquence du manque de lucidité et d’énergie, en dormant peu nous nous exposons à la diminution considérable de nos capacités. Ceux qui ne se reposent pas suffisamment ont tendance à donner moins de résultats dans les milieux professionnel.</p>
                                 </div>
                             }
                     </div>
